f86da62f6270f279da5dab3677a203ae743599cd
Dhanesh NeelaMana <dmana@amazon.com>
2018-09-09 16:05:46 -0400

    refactor

--- app/jobs/download_articles_job.rb
+++ app/jobs/download_articles_job.rb
@@ -9,28 +9,32 @@ class DownloadArticlesJob < ApplicationJob
 
   def perform(*args)
     # Do something later
-    Feed.all.each do |feed|
-      rss = RSS::Parser.parse(open(feed.url).read, false)
-      rss.items.each do |item|
-        if( Article.find_by_link(item.link) == nil)
-          begin
-            content = text_content(item.link)
-            article = {
-              :link => item.link,
-              :title => item.title,
-              :description => URI.unescape(item.description).html_safe,
-              :text_content => content,
-              :sentiment => sentiments(content),
-              :sentiment_score =>  textmoods(content),
-              :feed_id => feed.id
-            }
-            Article.create(article)
-            sleep(1)
-          rescue
-            puts 'link failed #{item.link}'
-          end    
-        end
-      end  
+    begin
+      Feed.all.each do |feed|
+        rss = RSS::Parser.parse(open(feed.url).read, false)
+        rss.items.each do |item|
+          if( Article.find_by_link(item.link) == nil)
+            begin
+              content = text_content(item.link.strip)
+              article = {
+                :link => item.link.strip,
+                :title => item.title,
+                :description => URI.unescape(item.description).html_safe,
+                :text_content => content,
+                :sentiment => sentiments(content),
+                :sentiment_score =>  textmoods(content),
+                :feed_id => feed.id
+              }
+              Article.create(article)
+              sleep(1)
+            rescue
+              puts 'link failed'
+            end    
+          end
+        end  
+      end
+    rescue
+      puts 'feed failed'    
     end
   end
 
