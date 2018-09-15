5cfc9f749f3047990169df91d80757cbb28aed96
Dhanesh NeelaMana <dmana@amazon.com>
2018-09-15 08:33:32 -0400

    Added sentiments in erb

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,12 @@
 class ArticlesController < ApplicationController
   def index
     @articles = Article.order(pub_date: :DESC, sentiment_score: :DESC).all
+    @feeds = Feed.all
+    @positive = Article.where(:sentiment => :positive)
+      .order(pub_date: :DESC, sentiment_score: :DESC).all
+    @negative = Article.where(:sentiment => :negative)
+      .order(pub_date: :DESC, sentiment_score: :DESC).all
+    @neutral = Article.where(:sentiment => :neutral)
+      .order(pub_date: :DESC, sentiment_score: :DESC).all
   end
 end
