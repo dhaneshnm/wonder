d23bcc913fc8d668fafba860def65b1de7d39b9a
Dhanesh NeelaMana <dmana@amazon.com>
2018-09-15 14:52:49 -0400

    style changes and article tabs

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,12 +1,24 @@
 class ArticlesController < ApplicationController
   def index
-    @articles = Article.order(pub_date: :DESC, sentiment_score: :DESC).all
-    @feeds = Feed.all
-    @positive = Article.where(:sentiment => :positive)
-      .order(pub_date: :DESC, sentiment_score: :DESC).all
-    @negative = Article.where(:sentiment => :negative)
-      .order(pub_date: :DESC, sentiment_score: :DESC).all
-    @neutral = Article.where(:sentiment => :neutral)
-      .order(pub_date: :DESC, sentiment_score: :DESC).all
+    if(params[:feed_id])
+      @articles = Article.where(:feed_id => params[:feed_id])
+        .order(pub_date: :DESC, sentiment_score: :DESC).all
+      @feeds = Feed.all
+      @positive = Article.where(:feed_id => params[:feed_id], :sentiment => :positive)
+        .order(pub_date: :DESC, sentiment_score: :DESC).all
+      @negative = Article.where(:feed_id => params[:feed_id], :sentiment => :negative)
+        .order(pub_date: :DESC, sentiment_score: :DESC).all
+      @neutral = Article.where(:feed_id => params[:feed_id], :sentiment => :neutral)
+        .order(pub_date: :DESC, sentiment_score: :DESC).all
+    else 
+      @articles = Article.order(pub_date: :DESC, sentiment_score: :DESC).all
+      @feeds = Feed.all
+      @positive = Article.where(:sentiment => :positive)
+        .order(pub_date: :DESC, sentiment_score: :DESC).all
+      @negative = Article.where(:sentiment => :negative)
+        .order(pub_date: :DESC, sentiment_score: :DESC).all
+      @neutral = Article.where(:sentiment => :neutral)
+        .order(pub_date: :DESC, sentiment_score: :DESC).all
+    end    
   end
 end
